097ad95daeba1c7cf7c60a2141595a7ed511cb35
Matthew Walker <mwalker@kydance.net>
2012-10-19 13:52:35 -0600

    Added the report command for users

--- src/SkynetBot/UserCommandListener.java
+++ src/SkynetBot/UserCommandListener.java
@@ -12,7 +12,18 @@
  */
 package SkynetBot;
 
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.pircbotx.Channel;
 import org.pircbotx.Colors;
+import org.pircbotx.User;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
@@ -38,6 +49,17 @@ public class UserCommandListener extends ListenerAdapter  {
 
 			if (command.equals("lastseen")) {
 				event.respond(SkynetBot.db.getLastSeen(args[2], event.getChannel()));
+			} else if (command.equals("report")) {
+				String reason = null;
+				if (args.length > 2) {
+					reason = "";
+					for (int i = 2; i < args.length; i++) {
+						reason += args[i] + " "; 
+					}
+				}
+				
+				sendLog(event.getChannel(), event.getUser(), reason);
+				event.respond("Thank you, informant! Your information is invaluable. This will be taken into consideration when it is time for your termination.");
 			} else if (command.equals("help")) {
 				printCommandList(event);
 			} else {
@@ -50,10 +72,57 @@ public class UserCommandListener extends ListenerAdapter  {
 		SkynetBot.bot.sendAction(event.getChannel(), "whispers something to " + event.getUser().getNick() + ". (Check for a new window or tab with the help text.)");
 
 		String[] helplines = {"Core Skynet User Commands:",
-							  "    $skynet lastseen <user> - Report when that user was last seen in channel",};
+							  "    !skynet lastseen <user>   - Report when that user was last seen in channel",
+							  "    !skynet report [<reason>] - Send last 25 lines of history to ML, with an optional reason",};
 
 		for (int i = 0; i < helplines.length; ++i) {
 			SkynetBot.bot.sendNotice(event.getUser(), helplines[i]);
+		}
+	}
+
+	protected void sendLog( Channel channel, User user, String explanation ) {
+		ChannelInfo info = SkynetBot.db.channel_data.get(channel.getName());
+
+		String from = "Skynet Bot <mwalker+nanowrimo@kydance.net>";
+		String host = "localhost";
+		
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.smtp.host", host);
+		
+		Session session = Session.getDefaultInstance(properties);
+		
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			
+			for (String ml : info.mls) {
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(SkynetBot.db.getMLEmail(channel, ml)));
+			}
+
+			message.addRecipient(Message.RecipientType.CC, new InternetAddress("Matthew Walker <mwalker+nanowrimo@kydance.net>"));
+			message.setSubject("Channel Log from Skynet");
+			
+			String body;
+
+			body = "A user on your channel by the name of '" + user.getNick() + "' requested that I send you this log.";
+			
+			if (explanation != null) {
+				body += " They provided this explanation:\n\n";
+				body += explanation + "\n\n";
+			}
+			
+			body += "Here is the activity leading up to the request:\n\n";
+
+			String[] logLines = ServerListener.channel_logs.get(channel.getName()).toArray(new String[0]);
+			for (String line : logLines) {
+				body += line + "\n";
+			}
+
+			message.setText(body);
+			
+			Transport.send(message);
+		} catch (MessagingException ex) {
+			Logger.getLogger(ServerListener.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 }
